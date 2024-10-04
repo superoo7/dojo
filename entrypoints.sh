a8487c5050ade15ef6ef00129bd084e981d08ebb
jarvis8x7b <157810922+jarvis8x7b@users.noreply.github.com>
2024-10-04 21:43:44 +0800

    refactor: extract miner services to separate compose

--- entrypoints.sh
+++ entrypoints.sh
@@ -12,36 +12,18 @@ if [ "$1" = 'dojo-cli' ]; then
     dojo
 fi
 
-if [ "$1" = 'miner-testnet' ]; then
+if [ "$1" = 'miner' ]; then
     echo "Environment variables:"
     echo "WALLET_COLDKEY: ${WALLET_COLDKEY}"
     echo "WALLET_HOTKEY: ${WALLET_HOTKEY}"
     echo "AXON_PORT: ${AXON_PORT}"
     echo "SUBTENSOR_NETWORK: ${SUBTENSOR_NETWORK}"
     echo "SUBTENSOR_ENDPOINT: ${SUBTENSOR_ENDPOINT}"
-
-    python main_miner.py \
-    --netuid 98 \
-    --subtensor.network ${SUBTENSOR_NETWORK} \
-    --subtensor.chain_endpoint ${SUBTENSOR_ENDPOINT} \
-    --logging.debug \
-    --wallet.name ${WALLET_COLDKEY} \
-    --wallet.hotkey ${WALLET_HOTKEY} \
-    --axon.port ${AXON_PORT} \
-    --neuron.type miner
-fi
-
-if [ "$1" = 'miner-mainnet' ]; then
-    echo "Environment variables:"
-    echo "WALLET_COLDKEY: ${WALLET_COLDKEY}"
-    echo "WALLET_HOTKEY: ${WALLET_HOTKEY}"
-    echo "AXON_PORT: ${AXON_PORT}"
-    echo "SUBTENSOR_NETWORK: ${SUBTENSOR_NETWORK}"
-    echo "SUBTENSOR_ENDPOINT: ${SUBTENSOR_ENDPOINT}"
+    echo "NETUID: ${NETUID}"
 
     # TODO change netuid before going live
     python main_miner.py \
-    --netuid 51 \
+    --netuid ${NETUID} \
     --subtensor.network ${SUBTENSOR_NETWORK} \
     --subtensor.chain_endpoint ${SUBTENSOR_ENDPOINT} \
     --logging.debug \
@@ -52,36 +34,18 @@ if [ "$1" = 'miner-mainnet' ]; then
 fi
 
 # If the first argument is 'validator', run the validator script
-if [ "$1" = 'validator-testnet' ]; then
+if [ "$1" = 'validator' ]; then
     echo "Environment variables:"
     echo "WALLET_COLDKEY: ${WALLET_COLDKEY}"
     echo "WALLET_HOTKEY: ${WALLET_HOTKEY}"
     echo "AXON_PORT: ${AXON_PORT}"
     echo "SUBTENSOR_NETWORK: ${SUBTENSOR_NETWORK}"
     echo "SUBTENSOR_ENDPOINT: ${SUBTENSOR_ENDPOINT}"
-
-    python main_validator.py \
-    --netuid 98 \
-    --subtensor.network ${SUBTENSOR_NETWORK} \
-    --subtensor.chain_endpoint ${SUBTENSOR_ENDPOINT} \
-    --logging.debug \
-    --wallet.name ${WALLET_COLDKEY} \
-    --wallet.hotkey ${WALLET_HOTKEY} \
-    --neuron.type validator \
-    --wandb.project_name dojo-testnet
-fi
-
-if [ "$1" = 'validator-mainnet' ]; then
-    echo "Environment variables:"
-    echo "WALLET_COLDKEY: ${WALLET_COLDKEY}"
-    echo "WALLET_HOTKEY: ${WALLET_HOTKEY}"
-    echo "AXON_PORT: ${AXON_PORT}"
-    echo "SUBTENSOR_NETWORK: ${SUBTENSOR_NETWORK}"
-    echo "SUBTENSOR_ENDPOINT: ${SUBTENSOR_ENDPOINT}"
+    echo "NETUID: ${NETUID}"
 
     # TODO change netuid before going live
     python main_validator.py \
-    --netuid 51 \
+    --netuid ${NETUID} \
     --subtensor.network ${SUBTENSOR_NETWORK} \
     --subtensor.chain_endpoint ${SUBTENSOR_ENDPOINT} \
     --logging.debug \
